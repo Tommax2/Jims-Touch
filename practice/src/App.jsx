70d388932232ccea1bdf1b3a263beee971a9301b
Tommax2 <martinsolumi@gmail.com>
2025-03-06 16:08:28 +0100

    add

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 
-import './App.css'
-import { Navbar } from './Components/Navbar/Navbar'
+import "./App.css";
+import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
-  
-
   return (
     <>
       <div>
         <Navbar />
-       </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- practice/src/Components/Navbar/Navbar.jsx
+++ practice/src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import './Navbar.css'
-import shop from './assets/shop.png'
+import React from "react";
+import "./Navbar.css";
+import shop from "../../assets/shop.png"; // Adjust the path to the image
 
 export const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className='navbar__left'>
-            <img src={shop} alt="shop"/>
+    <div className="navbar">
+      <div className="navbar__left">
+        <img src={shop} alt="shop" />
+        <h1>jims</h1>
+      </div>
+        <ul className="nav-menu">
+            <li>
+                Shop <hr />
+        
+            </li>
+            <li>
+                About
+            </li>
+            <li>
+                Contact
+            </li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={shop} alt="cart" />
             </div>
-            
     </div>
-    <
-  )
-}
+  );
+};
