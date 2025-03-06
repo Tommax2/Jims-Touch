a3c0acf6debc17fa3fd011605f6b6d7c57aa41fa
Tommax2 <martinsolumi@gmail.com>
2025-03-06 15:32:58 +0100

    add

---
+++ practice/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+import shop from './assets/shop.png'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='navbar__left'>
+            <img src={shop} alt="shop"/>
+            </div>
+            
+    </div>
+    <
+  )
+}
